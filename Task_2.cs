d45de5f169097a9a3917ab2d4dac873450f40d7e
pavellyub <107138984+pavellyub@users.noreply.github.com>
2022-06-13 18:37:27 +0500

    Add files via upload
    Second Task

---
+++ Task_2.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+int number = Input ("Введите ваше число: ");
+int secondDigit = 0;
+
+if (number < 99)
+{
+    Console.WriteLine("Ошибка. В числе нет третьей цифры");
+}
+else
+{
+    while (number > 999)   
+        {
+            number = number / 10;
+        }
+    secondDigit = number % 10;
+    Console.WriteLine($"Третья цифра заданного числа: {secondDigit}");    
+}
+
+int Input (string output)
+{
+    Console.WriteLine(output);
+    return Convert.ToInt32(Console.ReadLine());
+}
